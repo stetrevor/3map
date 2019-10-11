5b64cdfc39bc430b352a92243f3fbe3772706b91
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-11 19:26:26 +0800

    feat: Add Playground view for dev

---
+++ src/views/Playground.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="playground">
+    <h1>This is the playground page</h1>
+    <router-link to="layout-algorithm">Layout Algorithm</router-link>
+
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "playground"
+};
+</script>
