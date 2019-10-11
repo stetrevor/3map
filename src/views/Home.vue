dd5983599f9a6c6189f91dc7a5f6d82c6931ad9c
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-11 19:26:11 +0800

    feat: Add Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <tree-editor />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TreeEditor from "@/components/TreeEditor.vue";
+
+export default {
+  name: "home",
+  components: {
+    TreeEditor
+  }
+};
+</script>
