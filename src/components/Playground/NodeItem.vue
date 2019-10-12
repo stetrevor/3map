487f1021debb0f4e9b1c47fad92c4b5d4784de1b
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-12 14:36:19 +0800

    feat: Create Playground NodeItem

---
+++ src/components/Playground/NodeItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="pg-node-item">
+    <div class="pg-node-item__self" :style="getStyleObject(node)">
+      {{ node.id }}
+    </div>
+
+    <node-item v-for="node in node.children" :key="node.id" :node="node" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "node-item",
+
+  props: {
+    node: {
+      type: Object,
+      required: true
+    }
+  },
+
+  methods: {
+    getStyleObject(node) {
+      const { x, y, width, height } = node;
+      return {
+        left: `${x}px`,
+        top: `${y}px`,
+        width: `${width}px`,
+        height: `${height}px`
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.pg-node-item {
+  &__self {
+    position: absolute;
+    background-color: lightgray;
+    box-shadow: inset 0 0 0 1px black;
+    font-size: 4px;
+    padding-left: 2px;
+  }
+}
+</style>
