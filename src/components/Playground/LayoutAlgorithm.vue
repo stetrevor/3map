f5b7a370883c995eecda0756fa92fb2f06ff9627
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-12 12:58:10 +0800

    feat: Test layout library locally

--- src/components/Playground/LayoutAlgorithm.vue
+++ src/components/Playground/LayoutAlgorithm.vue
@@ -3,3 +3,16 @@
     Layout Algorithm
   </div>
 </template>
+
+<script>
+import { layout, Tree } from "non-layered-tidy-tree-layout";
+
+export default {
+  created() {
+    console.log("layout", layout, "Tree", Tree);
+    const tree = new Tree(10, 5, 0, []);
+    layout(tree);
+    console.log(tree.x === 0, tree.y === 0);
+  }
+};
+</script>
