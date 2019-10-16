5f82d02a082789e9ffa84d1c0329c2e9345c3b79
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-16 12:20:27 +0800

    feat: Implement files vuex module

--- src/store/modules/files.js
+++ src/store/modules/files.js
@@ -1,8 +1,36 @@
-const state = {};
-const mutations = {};
+import api from "@/api/local-storage.js";
+
+const GET_ALL_FILES = "GET_ALL_FILES";
+
+const state = {
+  fileList: []
+};
+
+const mutations = {
+  [GET_ALL_FILES](state, { files }) {
+    state.fileList = files;
+  }
+};
+
 const actions = {
-  getFiles({ commit }) {
-    console.log("getFiles");
+  async getAllFiles({ commit }) {
+    const files = await api.getAllFiles();
+    files.sort((a, b) => b.lastModified - a.lastModified);
+    commit(GET_ALL_FILES, { files });
+  },
+
+  async updateFile({ dispatch }, payload) {
+    await api.updateFile(payload);
+    return dispatch("getAllFiles");
+  },
+
+  async deleteFile({ dispatch }, payload) {
+    await api.deleteFile(payload);
+    return dispatch("getAllFiles");
+  },
+
+  async newFile() {
+    return api.newFile({ name: "Untitled" });
   }
 };
 
