0691ba6fb610b075942fb056281e3adbb24ef8bd
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-19 18:14:10 +0800

    Experiment with groupBy and switchMap

--- src/components/Playground/FirebaseCloud.vue
+++ src/components/Playground/FirebaseCloud.vue
@@ -8,19 +8,91 @@
       <h4>{{ p.refPath }}</h4>
       <progress max="100" :value="p.progress" />
       <a :href="p.downloadURL" v-if="p.downloadURL">{{ p.downloadURL }}</a>
-  </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { Subject, interval } from "rxjs";
+import {
+  flatMap,
+  switchMap,
+  groupBy,
+  tap,
+  map,
+  take,
+  filter
+} from "rxjs/operators";
+
+import { mapState, mapGetters, mapActions } from "vuex";
 
 export default {
   name: "firebase-cloud",
 
-  computed: mapState({ ongoingUploads: state => state.sync.ongoingUploads }),
+  computed: {
+    ...mapState({
+      ongoingUploads: state => state.sync.ongoingUploads
+    }),
+    ...mapGetters(["progress"])
+  },
 
-  methods: mapActions(["uploadFiles"])
+  methods: {
+    ...mapActions(["uploadFiles"]),
+    sendNext() {
+      this.subject.next({ id: this.id });
+    }
+  },
+
+  data() {
+    return {
+      id: 0
+    };
+  },
+
+  created() {
+    const subject = new Subject();
+    this.subject = subject;
+
+    let c1 = 0;
+    let c2 = 0;
+    const values = [
+      { id: 0, value: 0 },
+      { id: 1, value: 1 },
+      { id: 2, value: 2 },
+      { id: 0, value: 10 },
+      { id: 0, value: 20 },
+      { id: 0, value: 30 },
+      { id: 0, value: 40 }
+    ];
+    const source$ = interval(2000).pipe(
+      take(7),
+      map(i => values[i])
+    );
+
+    source$
+      .pipe(
+        tap(() => ++c1),
+        tap(() => console.log("c1", c1)),
+        groupBy(v => v.id),
+        tap(() => ++c2),
+        tap(() => console.log("c2", c2)),
+        flatMap(group$ =>
+          group$.pipe(
+            switchMap(v =>
+              interval(1000).pipe(
+                map(i => ({ value: v.value, index: i })),
+                take(10)
+              )
+            )
+          )
+        ),
+        tap(v => console.log("got value", v.index)),
+        filter(v => v.index === 9),
+        tap(() => --c2),
+        tap(() => console.log("c2 later", c2))
+      )
+      .subscribe();
+  }
 };
 </script>
 
