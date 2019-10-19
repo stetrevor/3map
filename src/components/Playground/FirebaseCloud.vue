4d8b2c6a69062c5565848245fb56456a06cfef92
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-19 15:07:08 +0800

    feat: Add FirebaseCloud.vue

---
+++ src/components/Playground/FirebaseCloud.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="firebase-cloud">
+    <input type="file" multiple @change="uploadFiles($event.target.files)" />
+
+    <h3>Uploading {{ ongoingUploads }} files</h3>
+  </div>
+</template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  name: "firebase-cloud",
+
+  computed: mapState({ ongoingUploads: state => state.sync.ongoingUploads }),
+
+  methods: mapActions(["uploadFiles"])
+};
+</script>
+
+<style lang="scss"></style>
