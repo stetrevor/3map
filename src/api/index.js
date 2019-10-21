454ffbcb228fe18a5e796edbe4a4eab7d61a7a9c
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-21 14:15:14 +0800

    Implement api.generateFileId

--- src/api/index.js
+++ src/api/index.js
@@ -6,4 +6,11 @@ import local from "./local-storage";
 export default {
   cloud,
   local,
+
+  /**
+   * Return a generated file identifier string for use in storage ref.
+   */
+  generateFileId() {
+    return shortid.generate();
+  }
 };
