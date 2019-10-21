a997db6910d3c6d278077f37ad069500ae3e0c24
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-21 14:13:16 +0800

    Add functions to tryout

--- src/components/Playground/CloudApi.vue
+++ src/components/Playground/CloudApi.vue
@@ -1,13 +1,78 @@
 <template>
   <div class="cloud-api">
-    Cloud API
+    <h3>Cloud API</h3>
+    <div class="cloud-api__demos">
+      <div>
+        <h4>Generate File Id: {{ fileId }}</h4>
+        <button @click="generateFileIdentifier">Generate</button>
+      </div>
+      <div>
+        <h4>Create a map file</h4>
+        <button @click="createMap">Create file</button>
+        <div>
+          Name: <span>{{ mapFile.name }}</span> refPath:
+          <span>{{ mapFile.refPath }}</span>
+        </div>
+      </div>
+      <button @click="uploadMap(map)">Upload a map</button>
+      <button>Download a map</button>
+      <button>List maps</button>
+      <button>Rename a map</button>
+      <button>Upload a map resource</button>
+      <button>Download a map resource</button>
+      <button>Delete a map resource</button>
+      <button>Rename a map resource</button>
+      <button>List map resources</button>
+    </div>
   </div>
 </template>
 
 <script>
+import api from "@/api";
+import { mapActions } from "vuex";
+
 export default {
-  name: "cloud-api"
+  name: "cloud-api",
+
+  methods: {
+    ...mapActions(["uploadMap", "new3MapFile"]),
+
+    generateFileIdentifier() {
+      this.fileId = api.generateFileIdentifier();
+    },
+
+    async createMap() {
+      this.mapFile = await this.new3MapFile();
+    }
+  },
+
+  data() {
+    return {
+      map: {
+        refPath: "get a refPath somewhere or set this as empty",
+        filename: "a whole new map",
+        string: JSON.stringify({
+          id: 0,
+          x: 0,
+          y: 0,
+          width: 100,
+          height: 50,
+          text: "Hello World",
+          children: []
+        })
+      },
+      fileId: null,
+      mapFile: { name: "", refPath: "" }
+    };
+  }
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.cloud-api {
+  &__demos {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
