392d68b42f6068dc5d7a05f9b4900078fec28291
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-22 17:13:41 +0800

    Implement list-files vuex plugin

---
+++ src/store/plugins/list-files.js
@@ -0,0 +1,27 @@
+import { Subject } from "rxjs";
+import { flatMap } from "rxjs/operators";
+
+import api from "@/api";
+import { UPDATE_NEXT_PAGE_TOKEN } from "../mutation-types";
+
+export default function createListFilesPlugin() {
+  return store => {
+    const source$ = new Subject();
+
+    const fileItems$ = source$.pipe(flatMap(api.cloud.nextPageMapFiles));
+
+    fileItems$.subscribe(item => {
+      if ("nextPageToken" in item) {
+        store.commit(UPDATE_NEXT_PAGE_TOKEN, item);
+      } else {
+        store.dispatch("addToMapFileList", item);
+      }
+    });
+
+    store.subscribeAction(action => {
+      if (action.type === "getNextPageMapFiles") {
+        source$.next(action.payload);
+      }
+    });
+  };
+}
