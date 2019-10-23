2032efbbc6d259e1fd5d6daf5a13a9c34e95f6f5
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-23 10:05:19 +0800

    Disable all local db api

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 import shortid from "shortid";
 
 import cloud from "./firebase";
-import local from "./local";
+// import local from "./local";
 
 export default {
   cloud,
-  local,
+  // local,
 
   /**
    * Return a generated file identifier string for use in storage ref.
