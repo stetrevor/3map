211f050e9de10043807a3e638e666ed86ccdfda0
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-24 10:37:42 +0800

    Use list-files plugin for vuex
    Github issue #3

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
+
 import files from "./modules/files";
 import editor from "./modules/editor";
 import sync from "./modules/sync";
+import createListFilesPlugin from "./plugins/list-files";
 
 Vue.use(Vuex);
 
@@ -12,6 +14,9 @@ const store = new Vuex.Store({
     editor,
     sync
   },
+
+  plugins: [createListFilesPlugin()],
+
   strict: process.env.NODE_ENV !== "production"
 });
 
