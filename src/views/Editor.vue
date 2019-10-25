e1e3d0905c38371ad1d27731c8f2041c7a12a8c0
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-25 14:54:07 +0800

    Create Editor view
    Github issue #15

---
+++ src/views/Editor.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="editor">
+    <tree-editor :tree="tree" :bounding-box="boundingBox" v-if="ready" />
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+import TreeEditor from "@/components/TreeEditor";
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "editor",
+
+  components: { TreeEditor },
+
+  computed: mapGetters(["tree", "boundingBox"]),
+
+  methods: {
+    ...mapActions(["getMapContent", "uploadMapFile"]),
+
+    async setup() {
+      this.ready = false;
+      await this.getMapContent(this.$route.params);
+      this.ready = true;
+    }
+  },
+
+  beforeRouteEnter(to, from, next) {
+    next(vm => vm.setup());
+  },
+
+  beforeRouteUpdate(to, from, next) {
+    this.setup();
+    next();
+  },
+
+  beforeRouteLeave(to, from, next) {
+    this.uploadMapFile(from.params);
+    next();
+  },
+
+  data() {
+    return {
+      ready: false
+    };
+  }
+};
+</script>
+
+<style lang="scss"></style>
