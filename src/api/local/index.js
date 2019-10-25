1b489dcde91d0d3eae79f5249391c8b07261e297
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-25 14:22:24 +0800

    Remove unused functions in local api
    Github issue #14

--- src/api/local/index.js
+++ src/api/local/index.js
@@ -1,5 +1,4 @@
 import { openDB } from "idb";
-import shortid from "shortid";
 
 const dbPromise = openDB("3map-app", 1, {
   upgrade(db) {
@@ -24,65 +23,6 @@ function fileToArrayBuffer(file) {
 }
 
 export default {
-  /**
-   * Return created new map file.
-   */
-  async newMapFile() {
-    const refPath = shortid.generate() + "/index.json";
-    const filename = "Untitled";
-    const content = {};
-    const item = { refPath, filename, content };
-    await (await dbPromise).put("files", item);
-    return item;
-  },
-
-  /**
-   * Return a URL of the resource file that can be used in <img> src.
-   *
-   * @param {string} mapRefPath The refPath of the map file this resource belong to
-   * @param {object} file Most likely a file object from <input>
-   */
-  async addMapResource({ mapRefPath, file }) {
-    const refPath = mapRefPath.replace("index.json", file.name);
-    const content = await fileToArrayBuffer(file);
-    const contentType = file.contentType;
-    const item = { mapRefPath, refPath, content, contentType };
-    await (await dbPromise).put("resources", item);
-    return URL.createObjectURL(file);
-  },
-
-  /**
-   * Return the updated map file record.
-   *
-   * @param {string} refPath The refPath of the file
-   * @param {object} updates The rest of the update object
-   */
-  async updateMapFile({ refPath, ...updates }) {
-    const db = await dbPromise;
-    const file = db.get("files", refPath);
-    const updated = Object.assign(file, updates);
-    await db.put("files", updated);
-    return updated;
-  },
-
-  /**
-   * Delete a file record.
-   *
-   * @param {string} refPath The refPath of the file to be deleted
-   */
-  async removeMapFile({ refPath }) {
-    (await dbPromise).delete("files", refPath);
-  },
-
-  /**
-   * Delete a resource file record.
-   *
-   * @param {string} refPath The refPath of the resource to be deleted
-   */
-  async removeMapResource({ refPath }) {
-    (await dbPromise).delete("resources", refPath);
-  },
-
   /**
    * Save the current state of map locally.
    * This includes map file id and filename for uploading convenience.
