f05157ec5691d8f1c63bf14e591edf42750ffe15
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-26 12:02:29 +0800

    Add workbox instance to Vue.prototype
    Github issue #18

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import './registerServiceWorker'
+import wb from "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$workbox = wb;
 
 new Vue({
   router,
