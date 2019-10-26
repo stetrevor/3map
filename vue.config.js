ae155d626ac601f88b86e90d3586af238fd0c526
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-26 11:57:29 +0800

    Add GenerateSW plugin to webpack
    Github issue #18

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+const { GenerateSW } = require("workbox-webpack-plugin");
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "development" ? "/3map/" : ""
+  publicPath: process.env.NODE_ENV === "development" ? "/3map/" : "",
+
+  configureWebpack: {
+    plugins: [new GenerateSW()]
+  }
 };
