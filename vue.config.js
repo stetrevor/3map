c12ee509644de5dac6ad815f891f6fbac822d832
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-27 11:42:54 +0800

    workbox config to support background sync
    Github issue #20

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,24 @@ module.exports = {
   publicPath: process.env.NODE_ENV === "development" ? "/3map/" : "",
 
   configureWebpack: {
-    plugins: [new GenerateSW()]
+    plugins: [
+      new GenerateSW({
+        runtimeCaching: [
+          {
+            // urlPattern: new RegExp("^https://firebasestorage.googleapis.com"),
+            urlPattern: /^https:\/\/firebasestorage\.googleapis\.com/,
+            handler: "NetworkFirst",
+            options: {
+              backgroundSync: {
+                name: "3map-app-background-sync-queue",
+                options: {
+                  maxRetentionTime: 60 * 24
+                }
+              }
+            }
+          }
+        ]
+      })
+    ]
   }
 };
